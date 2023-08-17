7cf86d10e14ff2765576ada231671123f5124be5
Akorede Adewole <akoredeadewole8@gmail.com>
2023-08-17 13:26:01 +0100

    add: added NeuralNetwork.cpp,

---
+++ sine_model/src/NeuralNetwork.cpp
@@ -0,0 +1,56 @@
+#include "NeuralNetwork.h"
+#include "sine_model.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+
+
+NeuralNetwork::NeuralNetwork()
+{
+    // error reporter for debugging
+    error_reporter = new tflite::MicroErrorReporter();
+
+    // map the model and check the version
+    model = tflite::GetModel(sine_model_tflite);
+
+    if (model-> version() != TFLITE_SCHEMA_VERSION)
+    {
+        TF_LITE_REPORT_ERROR(error_reporter, "Model provided's schema version %d not equal to supported version%d.", model-> version(), TFLITE_SCHEMA_VERSION);
+        return;
+    }
+
+    // resolver
+    resolver = new tflite::MicroMutableOpResolver<10>();
+    resolver->AddFullyConnected();
+    resolver->AddMul();
+    resolver->AddAdd();
+    resolver->AddLogistic();
+    resolver->AddReshape();
+    resolver->AddQuantize();
+    resolver->AddDequantize();
+
+    // tensor arena
+    const int tensor_arena_size = 2 * 1024;
+    uint8_t tensor_arena[tensor_arena_size];
+
+    if (!tensor_arena){
+        TF_LITE_REPORT_ERROR(error_reporter, "Could not allocate arena");
+        return;
+    }
+
+    // build the interpreter
+    interpreter = new tflite::MicroInterpreter(model, *resolver, tensor_arena, tensor_arena_size, error_reporter);
+
+    // allocate tensors
+    TfLiteStatus allocate_status = interpreter->AllocateTensors();
+    if (allocate_status != kTfLiteOk){
+        TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+        return;
+    }
+
+    // inspect input tensor
+    input = interpreter->input(0);
+    output = interpreter->output(0);
+
+}
--- sine_model/src/NeuralNetwork.h
+++ sine_model/src/NeuralNetwork.h
@@ -2,5 +2,33 @@
 #define __NeuralNetwork__
 
 
+namespace tflite{
+    template <unsigned int tOpCount>
+    class MicroMutableOpResolver;
+    class ErrorReporter;
+    class Model;
+    class MicroInterpreter;
+}
+
+struct TfLiteTensor;
+
+class NeuralNetwork
+{
+    private:
+    tflite::ErrorReporter *error_reporter;
+    const tflite::Model *model;
+    tflite::MicroInterpreter *interpreter;
+    tflite::MicroMutableOpResolver<10> *resolver;
+    TfLiteTensor *input;
+    TfLiteTensor *output;
+
+    public:
+    float *getInputBuffer();
+    NeuralNetwork();
+    float predict();
+
+};
+
+
 
 #endif
